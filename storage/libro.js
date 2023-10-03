f01b466a4f6450690f174c51ee277983a9a274f7
Johan Sebastian Pachon Mojica <111256873+johanp0308@users.noreply.github.com>
2023-10-03 07:34:10 -0500

    feat: :sparkles: Add Structure file

--- storage/libro.js
+++ storage/libro.js
@@ -1,8 +1,34 @@
 import {env} from '../config.js';
 const uri = `${env.ssl+env.hostName}:${env.port}`
-const config = {method:'',headers:{"content-type": "application/json"}};
+const config = {methods:'',headers:{"content-type": "application/json"}};   
 
-console.log(uri);
+const defaultObj = {
+    id:null,
+    autorId:null,
+    categoriaId:null,
+    editorialId:null,
+    titulo:"",
+    fechaLanzamiento:"",
+    isbn:"",
+    numPaginacion:undefined,
+    estadoId:undefined
+}
+
+const validLibro = (obj) => {
+    const {id, autorId, categoriaId, editorialId, titulo, fechaLanzamiento, isbn, numPaginacion, estadoId} = obj;
+    let date = new Date(fechaLanzamiento);
+    if(!(date && date.getFullYear()<=2040)) return {status: 400, message: `El dato fecha: '${fechaLanzamiento}' no cumple con el formato`};
+    console.log("la validacion sigue")
+    if(typeof id !== 'number') return {status: 400, message: `El dato id: '${id}' no cumple con el formato`};
+    if(typeof autorId !== 'number') return {status: 400, message: `El dato autorId: '${autorId}' no cumple con el formato`};
+    if(typeof categoriaId !== 'number') return {status: 400, message: `El dato categoriaId: '${categoriaId}' no cumple con el formato`};
+    if(typeof editorialId !== 'number') return {status: 400, message: `El dato editorialId: '${editorialId}' no cumple con el formato`};
+    if(typeof titulo !== 'string') return {status: 400, message: `El dato titulo:'${titulo}' no cumple con el formato`};
+    if(typeof isbn !== 'string') return {status: 400, message: `El dato isbn: '${isbn}' no cumple con el formato`};
+    if(typeof numPaginacion !== 'number') return {status: 400, message: `El dato numPaginacion: '${numPaginacion}' no cumple con el formato`};
+    if(typeof estadoId !== 'number') return {status: 400, message: `El dato estadoId: '${estadoId}' no cumple con el formato`};
+    return false;
+}
 
 export const getAll = async() =>{
     config.methods = 'GET'
@@ -21,24 +47,14 @@ export const deletOne = async(id) =>{
     let res = await (await fetch(`${uri}/libro/${id}`,config)).json();
     return res;
 }
-export const putOne = async(obj={id,autorId,categoriaId,editorialId,titulo,fechaLanzamiento,isbn,numPaginacion,estadoId}) =>{
-    const {id, autorId, categoriaId, editorialId, titulo, fechaLanzamiento, isbn, numPaginacion, estadoId} = obj;
-    let date = new Date(fechaLanzamiento);
-    if(!(date && date.getFullYear()<=2040)) return {status: 400, message: `El datos '${fechaLanzamiento}' no cumple con el formato`};
-    if(typeof id !== 'number') return {status: 400, message: `El datos '${id}' no cumple con el formato`};
-    if(typeof autorId !== 'number') return {status: 400, message: `El datos '${autorId}' no cumple con el formato`};
-    if(typeof categoriaId !== 'number') return {status: 400, message: `El datos '${categoriaId}' no cumple con el formato`};
-    if(typeof editorialId !== 'number') return {status: 400, message: `El datos '${editorialId}' no cumple con el formato`};
-    if(typeof titulo !== 'string') return {status: 400, message: `El datos '${titulo}' no cumple con el formato`};
-    if(typeof isbn !== 'string') return {status: 400, message: `El datos '${isbn}' no cumple con el formato`};
-    if(typeof numPaginacion !== 'number') return {status: 400, message: `El datos '${numPaginacion}' no cumple con el formato`};
-    if(typeof estadoId !== 'number') return {status: 400, message: `El datos '${estadoId}' no cumple con el formato`};
-    
-    // config.methods = 'PUT';
-    // config.body = JSON.stringify(obj);
-    // let res = await (await fetch(`${uri}/libro/${id}`,config)).json();
+export const putOne = async(obj=defaultObj) =>{
+    if(!validLibro(obj)) return obj
+    const {id} =obj;
+    config.methods = 'PUT';
+    config.body = JSON.stringify(obj);
+    let res = await (await fetch(`${uri}/libro/${id}`,config)).json();
 
-    return "Hola"
+    return "Hola";
 }
 
 console.log(putOne());
