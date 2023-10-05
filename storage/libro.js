b5d8f405111ba1aa430bcc9c7d10e0389289d116
johanp0308 <sebastian-johan1@hotmail.com>
2023-10-04 22:38:52 -0500

    feat: :technologist: Adding relationships and testing even without finishing
    Not executing gives Error

--- storage/libro.js
+++ storage/libro.js
@@ -1,8 +1,15 @@
+import {getOne as getOneAutor} from './autor.js'
+import { getOne as getOneCategoria} from './categoria.js';
+import { getOne as getOneEditorial} from './editorial.js';
+
+
 import {env} from '../config.js';
+import { compareEstructure as compareObject } from '../tools/validations.js';
+
 const uri = `${env.ssl+env.hostName}:${env.port}`
 const config = {method:'',headers:{"content-type": "application/json"}};   
 
-const endpoint = 'libro'
+const endpoint = 'libros'
 
 const compareData = (data,dataC)=>{
     let dataNew = Object.keys(data).map(key=>{
@@ -17,14 +24,14 @@ const validLibro = (data) => {
         isbn=null, 
         numPaginacion=null, 
         estadoId=null}=data;
-    if(typeof data !== 'Object' || Object.keys(data)==0) return {status: 404, message:'Porfavor envie algun dato'}
+    if(data.constructor.name !== 'Object' || Object.keys(data)==0) return {status: 404, message:'Porfavor envie algun dato'}
     let date = new Date(fechaLanzamiento);
     if(!(date && date.getFullYear()<=2040)) return {status: 400, message: `El dato fecha: '${fechaLanzamiento}' no cumple con el formato`};
     if(typeof autorId !== 'number') return {status: 400, message: `El dato autorId: '${autorId}' no cumple con el formato`};
     if(typeof categoriaId !== 'number') return {status: 400, message: `El dato categoriaId: '${categoriaId}' no cumple con el formato`};
     if(typeof editorialId !== 'number') return {status: 400, message: `El dato editorialId: '${editorialId}' no cumple con el formato`};
     if(typeof titulo !== 'string') return {status: 400, message: `El dato titulo:'${titulo}' no cumple con el formato`};
-    if(typeof isbn !== 'string') return {status: 400, message: `El dato isbn: '${isbn}' no cumple con el formato`};
+    if(typeof isbn !== 'number') return {status: 400, message: `El dato isbn: '${isbn}' no cumple con el formato`};
     if(typeof numPaginacion !== 'number') return {status: 400, message: `El dato numPaginacion: '${numPaginacion}' no cumple con el formato`};
     if(typeof estadoId !== 'number') return {status: 400, message: `El dato estadoId: '${estadoId}' no cumple con el formato`};
     return data;
@@ -38,17 +45,16 @@ export const getAll = async() =>{
 
 export const getOne = async(id) =>{
     if(typeof id!== 'number') return  {status: 404, message:`${id} is not a number`};
-    config.method = 'GET';
-    let res = await (await fetch(`${uri}/${endpoint}/${id}`,config)).json();
+    config.method = 'GET';JSON.stringify(obj)
     return res;
 }
 
 export const post = async(obj={}) =>{
-    let valid = validCategoria(obj);    
+    let valid = validLibro(obj);    
     if(valid.status) return valid;
-
     config.method = 'POST'
     config.body = JSON.stringify(obj);
+    console.log(JSON.stringify(obj))
     let res = await (await fetch(`${uri}/${endpoint}`,config)).json();
     return res;
 }
@@ -59,17 +65,31 @@ export const deletOne = async(id) =>{
     return res;
 }
 export const putOne = async(obj={}) =>{
-    let valid = validCategoria(obj);    
+    let valid = validLibro(obj);    
     if(valid.status) return valid;
     const {id} = obj;
     if(typeof id !== 'number') return {status: 400, message: `El dato id: '${id}' no cumple con el formato`};
 
-    let objReal = compareData(obj, await getOne(id));
-
+    let newEdit = compareObject(await getOne(id),obj);
     config.method = 'PUT';
-    config.body = JSON.stringify(obj);
+    config.body = JSON.stringify(newEdit);
     let res = await (await fetch(`${uri}/${endpoint}/${id}`,config)).json();
-    console.log(res);
     return res;
-    
 }
+
+export const  getRelationShips = async() =>{
+    let res = await getAll();
+    res = await res.map( async (data)=>{
+        let {categoriaId:idCAt, autorId:idAutor, editorialId:idEdit} = data;
+        console.log(idAutor,idCAt,idEdit);
+        
+        data.categoriaId = await getOneCategoria(idCAt);
+        data.autorId = await getOneAutor(autorId);
+        data.editorialId = await getOneEditorial(idEdit);
+        
+        return data;
+    });
+    console.log(res);
+}
+
+getRelationShips();
